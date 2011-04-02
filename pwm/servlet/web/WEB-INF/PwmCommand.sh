7cf0a9b29c2c5df2e6275c7e2bb82bbee6be26aa
mweisberg <mweisberg@gmail.com>
2011-04-02 02:16:51 +0000

    Moved CLASSPATH to after JAVA_HOME check.

--- pwm/servlet/web/WEB-INF/PwmCommand.sh
+++ pwm/servlet/web/WEB-INF/PwmCommand.sh
@@ -6,11 +6,11 @@
 #
 # Krowten's fault
 
-CLASSPATH=$(for i in lib/*.jar ; do echo -n $i: ; done).:classes
-
 if [ -z "$JAVA_HOME" ]; then
 echo "JAVA_HOME variable must be set to a valid Java JDK or JRE"
 exit 1
 fi
+
+CLASSPATH=$(for i in lib/*.jar ; do echo -n $i: ; done).:classes
 
 $JAVA_HOME/jre/bin/java -cp $CLASSPATH password.pwm.util.MainClass $1 $2 $3 $4 $5 $6 $7 $8 $9
