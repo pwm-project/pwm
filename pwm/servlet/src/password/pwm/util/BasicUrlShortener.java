1cd7295311e0e8d821254f371838e9437c2a8b47
jrivard <jrivard@gmail.com>
2012-01-26 23:30:40 +0000

    Copyright 2012 Update

--- pwm/servlet/src/password/pwm/util/BasicUrlShortener.java
+++ pwm/servlet/src/password/pwm/util/BasicUrlShortener.java
@@ -3,7 +3,7 @@
  * http://code.google.com/p/pwm/
  *
  * Copyright (c) 2006-2009 Novell, Inc.
- * Copyright (c) 2009-2011 The PWM Project
+ * Copyright (c) 2009-2012 The PWM Project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package password.pwm.util; 
+package password.pwm.util;
+ 
 import java.util.Properties;
 import password.pwm.util.AbstractUrlShortener;
 import password.pwm.PwmApplication;
