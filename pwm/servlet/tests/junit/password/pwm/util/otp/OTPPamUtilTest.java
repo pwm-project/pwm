d913486603a961e9bc4b43486e7b0f368a21556e
eters <menno.pieters@gmail.com>
2014-05-15 07:55:51 +0000

    Unit test classes for OTP utility classes...

---
+++ pwm/servlet/tests/junit/password/pwm/util/otp/OTPPamUtilTest.java
@@ -0,0 +1,120 @@
+/*
+ * Password Management Servlets (PWM)
+ * http://code.google.com/p/pwm/
+ *
+ * Copyright (c) 2006-2009 Novell, Inc.
+ * Copyright (c) 2009-2014 The PWM Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package password.pwm.util.otp;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mpieters
+ */
+public class OTPPamUtilTest {
+    
+    public OTPPamUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of splitLines method, of class OTPPamUtil.
+     */
+    @Test
+    public void testSplitLines() {
+        System.out.println("splitLines");
+        String text = "TUC2JMV7BLJVV6YX\r\n\" WINDOW_SIZE -1\r\n\" TOTP_AUTH\r\n72706699\r\n";
+        List<String> result = OTPPamUtil.splitLines(text);
+        assertEquals(4, result.size());
+    }
+
+    /**
+     * Test of decomposePamData method, of class OTPPamUtil.
+     */
+    @Test
+    public void testDecomposePamData() {
+        System.out.println("decomposePamData");
+        // TOTP
+        String text = "TUC2JMV7BLJVV6YX\r\n\" WINDOW_SIZE -1\r\n\" TOTP_AUTH\r\n72706699\r\n";
+        OTPUserConfiguration otp = OTPPamUtil.decomposePamData(text);
+        assertNotNull(otp);
+        assertEquals("TUC2JMV7BLJVV6YX", otp.getSecret());
+        assertEquals(OTPUserConfiguration.Type.TOTP, otp.getType());
+        // HOTP
+        text = "D4GVE762TVEIHYJF\r\n\" HOTP_COUNTER 10\r\n72706699\r\n87839814\r\n";
+        otp = OTPPamUtil.decomposePamData(text);
+        assertNotNull(otp);
+        assertEquals("D4GVE762TVEIHYJF", otp.getSecret());
+        assertEquals(OTPUserConfiguration.Type.HOTP, otp.getType());
+        assertEquals(10L, otp.getCurrentCounter());
+        assertEquals(2, otp.getRecoveryCodes().size());
+    }
+
+    /**
+     * Test of composePamData method, of class OTPPamUtil.
+     */
+    @Test
+    @SuppressWarnings("empty-statement")
+    public void testComposePamData() {
+        System.out.println("composePamData");
+        // TOTP
+        OTPUserConfiguration otp = new OTPUserConfiguration();
+        otp.setType(OTPUserConfiguration.Type.TOTP);
+        otp.setSecret("TUC2JMV7BLJVV6YX");
+        String[] recoveryCodes = { "72706699", "87839814" };
+        otp.setRecoveryCodes(Arrays.asList(recoveryCodes));
+        String expResult = "TUC2JMV7BLJVV6YX\n\" TOTP_AUTH\n72706699\n87839814\n";
+        String result = OTPPamUtil.composePamData(otp);
+        assertEquals(expResult, result);
+        // HOTP
+        otp = new OTPUserConfiguration();
+        otp.setType(OTPUserConfiguration.Type.HOTP);
+        otp.setSecret("D4GVE762TVEIHYJF");
+        otp.setRecoveryCodes(Arrays.asList(recoveryCodes));
+        otp.setCounter(10L);
+        expResult = "D4GVE762TVEIHYJF\n\" HOTP_COUNTER 10\n72706699\n87839814\n";
+        result = OTPPamUtil.composePamData(otp);
+        assertEquals(expResult, result);
+        // HOTP
+    }
+    
+}
---
+++ pwm/servlet/tests/junit/password/pwm/util/otp/OTPUrlUtilTest.java
@@ -0,0 +1,73 @@
+/*
+ * Password Management Servlets (PWM)
+ * http://code.google.com/p/pwm/
+ *
+ * Copyright (c) 2006-2009 Novell, Inc.
+ * Copyright (c) 2009-2014 The PWM Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package password.pwm.util.otp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mpieters
+ */
+public class OTPUrlUtilTest {
+    
+    public OTPUrlUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of composeOtpUrl method and decomposeOtpUrl, of class OTPUrlUtil.
+     */
+    @Test
+    public void testComposeAndDecomposeOtpUrl() {
+        System.out.println("composeOtpUrl");
+        OTPUserConfiguration otp = new OTPUserConfiguration("TEST");
+        otp.setSecret("2222222222222222");
+        String result = OTPUrlUtil.composeOtpUrl(otp);
+        System.out.println(result);
+        OTPUserConfiguration xotp = OTPUrlUtil.decomposeOtpUrl(result);   
+        assertNotNull(xotp);
+        assertEquals(otp.getIdentifier(), xotp.getIdentifier());
+        assertEquals(otp.getSecret(), xotp.getSecret());
+    }
+    
+}
