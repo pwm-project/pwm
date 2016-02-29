1871d908338118e7001b0282a69297d050e148ae
Jason Rivard <jrivard@gmail.com>
2016-02-29 18:01:14 -0500

    update license year and add pom license info

--- src/main/java/password/pwm/config/stored/StoredConfigurationUtil.java
+++ src/main/java/password/pwm/config/stored/StoredConfigurationUtil.java
@@ -1,9 +1,9 @@
 /*
  * Password Management Servlets (PWM)
- * http://code.google.com/p/pwm/
+ * http://www.pwm-project.org
  *
  * Copyright (c) 2006-2009 Novell, Inc.
- * Copyright (c) 2009-2015 The PWM Project
+ * Copyright (c) 2009-2016 The PWM Project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 
 package password.pwm.config.stored;
