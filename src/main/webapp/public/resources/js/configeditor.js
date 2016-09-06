b329bec559be6e47c64239474c637b8bd9ed2d93
James Albright <jalbr74@gmail.com>
2016-09-06 09:06:44 -0600

    Added validation for email address in Email Settings

--- src/main/webapp/public/resources/js/configeditor.js
+++ src/main/webapp/public/resources/js/configeditor.js
@@ -909,6 +909,10 @@ PWM_CFGEDIT.initSettingDisplay = function(setting, options) {
             EmailTableHandler.init(settingKey);
             break;
 
+        case 'EMAIL_ADDRESS':
+            StringValueHandler.init(settingKey);
+            break;
+
         case 'ACTION':
             ActionHandler.init(settingKey);
             break;
