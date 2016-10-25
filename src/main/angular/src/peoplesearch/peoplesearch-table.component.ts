b1dc72e0e706451a13ca71fdbce5b701634b2430
Joe Hawkins <jedwardhawkins@gmail.com>
2016-10-25 13:44:40 -0600

    Refactored peoplesearch card and table views into common base class.

--- src/main/angular/src/peoplesearch/peoplesearch-table.component.ts
+++ src/main/angular/src/peoplesearch/peoplesearch-table.component.ts
@@ -1,46 +1,18 @@
 import { Component } from '../component';
 import { IScope } from 'angular';
+import PeopleSearchBaseComponent from './peoplesearch-base.component';
 import PeopleSearchService from './peoplesearch.service';
-import Person from '../models/person.model';
 
-declare var PWM_PS: any;
 
 @Component({
     stylesheetUrl: require('peoplesearch/peoplesearch-table.component.scss'),
     templateUrl: require('peoplesearch/peoplesearch-table.component.html')
 })
-export default class PeopleSearchTableComponent {
-    private deregistrationCallback: () => void;
-    people: Person[];
-
-    static $inject = [ '$scope', '$state', 'PeopleSearchService' ];
+export default class PeopleSearchTableComponent extends PeopleSearchBaseComponent {
+    static $inject = [ '$scope', 'PeopleSearchService' ];
     constructor(
-        private $scope: IScope,
-        private $state: angular.ui.IStateService,
-        private peopleSearchService: PeopleSearchService) {
-    }
-
-    $onInit() {
-        this.getPeople();
-
-        var self = this;
-        this.deregistrationCallback = this.$scope.$on('people-updated', () => {
-            self.getPeople();
-        });
-    }
-
-    $onDestroy() {
-        this.deregistrationCallback();
-    }
-
-    getPeople() {
-        this.people = this.peopleSearchService.people;
-    }
-
-    selectPerson(id: string) {
-        // this.$state.go('orgchart', { personId: id });
-
-        // TODO: This is here to temporarily pull up the old modal dialog:
-        PWM_PS.showUserDetail(id);
+        $scope: IScope,
+        peopleSearchService: PeopleSearchService) {
+        super($scope, peopleSearchService);
     }
 }
