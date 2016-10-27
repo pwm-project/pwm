8b1928334948b612a3eb3e95475da94fb9b3bf5c
James Albright <jalbr74@gmail.com>
2016-10-27 09:11:51 -0600

    Introduced photo URL for person card

--- src/main/angular/src/peoplesearch/person-card.component.ts
+++ src/main/angular/src/peoplesearch/person-card.component.ts
@@ -12,16 +12,19 @@ import Person from '../models/person.model';
 export default class PersonCardComponent {
     private person: Person;
     private data: string[];
+    private photoURL: string;
 
     static $inject = [];
     constructor() {
+        this.photoURL = this.person.photoURL;
+
         // This data is only available on people search views
         if (this.person.givenName && this.person.sn) {
             this.data = [
                 this.person.givenName + ' ' + this.person.sn,
                 this.person.title,
                 this.person.telephoneNumber,
-                this.person.mail,
+                this.person.mail
             ];
         }
         // This data is only available on details and orgchart views
