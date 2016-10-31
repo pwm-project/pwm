34d75790e468378a215fa374a4f33ac0af9cdb82
Joe Hawkins <jedwardhawkins@gmail.com>
2016-10-31 16:01:38 -0600

    OrgChart component handles overflow when there are too many managers to display on screen.

--- src/main/angular/src/peoplesearch/orgchart.component.ts
+++ src/main/angular/src/peoplesearch/orgchart.component.ts
@@ -9,11 +9,20 @@ import { IScope } from 'angular';
     templateUrl: require('peoplesearch/orgchart.component.html')
 })
 export default class OrgChartComponent {
-    private person: Person;
-    private emptyPerson: Person;
-    private managementChain: Person[];
-    private directReports: Person[];
+    directReports: Person[];
+    managementChain: Person[];
+    person: Person;
+    emptyPerson: Person = new Person({
+        displayNames: [
+            'No Managers'
+        ],
+        photoURL: 'images/question_mark.png',
+        userKey: null
+    });
+
+    private maxManagers: number;
     private windowWidth: number;
+    private managers: Person[];
 
     static $inject = ['$q', '$scope', '$state', '$stateParams', '$window', 'PeopleService'];
     constructor(
@@ -26,44 +35,25 @@ export default class OrgChartComponent {
     }
 
     $onInit() {
-        var personId: string = this.$stateParams['personId'];
+        // Retrieve data from the server
+        this.fetchData();
+        this.updateLayout();
+
         var self = this;
 
-        // Fetch data
-        if (personId) {
-            this.$q.all({
-                directReports: this.peopleService.getDirectReports(personId),
-                managementChain: this.peopleService.getManagementChain(personId),
-                person: this.peopleService.getPerson(personId)
-            })
-            .then((data) => {
-                this.$scope.$evalAsync(() => {
-                    self.directReports = data['directReports'];
-                    self.managementChain = data['managementChain'];
-                    self.person = data['person'];
-                });
-            })
-            .catch((result) => {
-                console.log(result);
-            });
-        }
-
-        // Setup listener on window resize
+        // OrgChartComponent has different functionality at different window widths. On window resize, we
+        // want to update the state of the component and trigger a $digest
         // noinspection TypeScriptUnresolvedFunction
-        element(this.$window).bind('resize', function() {
-            self.setWindowWidth();
+        element(this.$window).bind('resize', () => {
+            self.updateLayout();
             self.$scope.$apply();
         });
 
-        // Initialize windowWidth
-        this.setWindowWidth();
-
-        this.emptyPerson = new Person({
-            displayNames: [
-                'No Managers'
-            ],
-            photoURL: 'images/question_mark.png',
-            userKey: null
+        // In large displays managers are displayed in a row. Any time this property changes, we want
+        // to force our manager list to be recalculated in this.getManagementChain() so it returns the correct
+        // result at all window widths
+        this.$scope.$watch('$ctrl.maxManagers', () => {
+            this.resetManagerList();
         });
     }
 
@@ -76,12 +66,31 @@ export default class OrgChartComponent {
     }
 
     getManagementChain(): Person[] {
+        // Display managers in a row
         if (this.isWideLayout()) {
-            // return original data
-            return this.managementChain;
+            // All managers can fit on screen
+            if (this.maxManagers >= this.managementChain.length) {
+                return this.managementChain;
+            }
+
+            // Not all managers can fit on screen
+            if (!this.managers) {
+                // Show a blank manager as last manager in the chain in place of
+                // the last visible manager. Blank manager links to the new last visible manager.
+                this.managers = this.managementChain.slice(0, this.maxManagers - 1);
+                var lastManager = this.managementChain[this.maxManagers - 2];
+
+                this.managers.push(new Person({
+                    userKey: lastManager.userKey,
+                    photoURL: 'images/more.png',
+                    displayNames: []
+                }));
+            }
+
+            return this.managers;
         }
 
-        // return reversed data
+        // All managers can fit on screen in a column. Order is reversed for ease of rendering and style
         return [].concat(this.managementChain).reverse();
     }
 
@@ -93,7 +102,7 @@ export default class OrgChartComponent {
         return this.managementChain && this.managementChain.length;
     }
 
-    isOrphan() {
+    isPersonOrphan() {
         return !(this.hasDirectReports() || this.hasManagementChain());
     }
 
@@ -101,11 +110,51 @@ export default class OrgChartComponent {
         this.$state.go('orgchart', { personId: userKey });
     }
 
-    setWindowWidth() {
-        this.windowWidth = this.$window.innerWidth;
+    private fetchData() {
+        var personId: string = this.$stateParams['personId'];
+        var self = this;
+
+        // Fetch data
+        if (personId) {
+            this.$q.all({
+                directReports: this.peopleService.getDirectReports(personId),
+                managementChain: this.peopleService.getManagementChain(personId),
+                person: this.peopleService.getPerson(personId)
+            })
+                .then((data) => {
+                    this.$scope.$evalAsync(() => {
+                        self.directReports = data['directReports'];
+                        self.managementChain = data['managementChain'];
+                        self.person = data['person'];
+                    });
+                })
+                .catch((result) => {
+                    console.log(result);
+                });
+        }
     }
 
     private isWideLayout() {
         return this.windowWidth >= 625;
+    }
+
+    // Remove all displayed managers so the list is updated on window resize
+    private resetManagerList() {
+        this.managers = null;
+    }
+
+    private setMaxManagers() {
+        this.maxManagers = Math.floor(
+            (this.windowWidth - 115 /* left margin */) /
+            125 /* card width + right margin */);
+    }
+
+    private setWindowWidth() {
+        this.windowWidth = this.$window.innerWidth;
+    }
+
+    private updateLayout() {
+        this.setWindowWidth();
+        this.setMaxManagers();
     }
 }
