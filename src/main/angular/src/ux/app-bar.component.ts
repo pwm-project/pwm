64b2b1857ecd3fa17a16a3ca403b3096c0aa614b
Joe Hawkins <jedwardhawkins@gmail.com>
2016-11-07 10:30:57 -0700

    Common prefix added to all ux elements

--- src/main/angular/src/ux/app-bar.component.ts
+++ src/main/angular/src/ux/app-bar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '../component';
 
 @Component({
     stylesheetUrl: require('ux/app-bar.component.scss'),
-    template: `<div class="app-bar-content" ng-transclude></div>`,
+    template: `<div class="mf-app-bar-content" ng-transclude></div>`,
     transclude: true
 })
 export default class AppBarComponent {
--- src/main/angular/src/ux/ux.module.ts
+++ src/main/angular/src/ux/ux.module.ts
@@ -2,12 +2,14 @@ import { module } from 'angular';
 import AppBarComponent from './app-bar.component';
 import AutoCompleteComponent from './auto-complete.component';
 import IconButtonComponent from './icon-button.component';
+import TableComponent from './table.component';
 
 var moduleName = 'peoplesearch.ux';
 
 module(moduleName, [ ])
-    .component('appBar', AppBarComponent)
-    .component('autoComplete', AutoCompleteComponent)
-    .component('iconButton', IconButtonComponent);
+    .component('mfAppBar', AppBarComponent)
+    .component('mfAutoComplete', AutoCompleteComponent)
+    .component('mfIconButton', IconButtonComponent)
+    .component('mfTable', TableComponent);
 
 export default moduleName;
