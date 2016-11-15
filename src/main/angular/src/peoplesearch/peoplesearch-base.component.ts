b7be813da00754de6430946ff3d9875231505551
Joe Hawkins <jedwardhawkins@gmail.com>
2016-11-14 17:46:51 -0700

    Code review for 'No results' story.

--- src/main/angular/src/peoplesearch/peoplesearch-base.component.ts
+++ src/main/angular/src/peoplesearch/peoplesearch-base.component.ts
@@ -25,9 +25,9 @@ export default class PeopleSearchBaseComponent {
     }
 
     // Trigger "No Results" message when search already done loading has no results
-    noSearchResults(): boolean {
-        if (this.loading && this.query && this.query.length) {
-            return (this.people.length == 0);
+    hasSearchResults(): boolean {
+        if (this.query && !this.loading) {
+            return this.people.length > 0;
         }
 
         return false;
