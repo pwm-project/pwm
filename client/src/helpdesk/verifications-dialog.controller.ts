ebba608ac306519ed4900579ed2a0794b3bbc08e
jalbr74 <jalbr74@gmail.com>
2018-02-22 15:24:02 -0700

    Fixed a problem with not being able to validate a user's token sent via email.

--- client/src/helpdesk/verifications-dialog.controller.ts
+++ client/src/helpdesk/verifications-dialog.controller.ts
@@ -126,9 +126,9 @@ export default class VerificationsDialogController {
                     let choice = (tokenSendMethod === TOKEN_CHOICE) ? method : null;
                     return this.helpDeskService.sendVerificationToken(this.personUserKey, choice);
                 })
-                .then((response) => {
+                .then((response: any) => {
                     this.status = STATUS_VERIFY;
-                    this.tokenData = response;
+                    this.tokenData = response.data;
                 });
         }
         else if (method === VERIFICATION_METHOD_NAMES.OTP) {
