aa9caec3aaba037a65804d64be7eeb1b13aace26
jrivard@gmail.com <b515a3148018c8681addaab41d35d274e2008bc0>
2018-11-30 16:26:53 -0500

    fix typo in command.sh

--- docker/src/main/image-files/app/command.sh
+++ docker/src/main/image-files/app/command.sh
@@ -2,5 +2,5 @@
 # Run command line shell environment inside the docker container.  Execute using
 # docker exec -it <container name> /application/command.sh
 
-java -jar /app/libs/pwm-onejar*.jar -applicationPath /config -command $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -jar /app/libs/*onejar*.jar -applicationPath /config -command $1 $2 $3 $4 $5 $6 $7 $8 $9
 
