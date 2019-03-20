14e6e1acf52ad6749012d947a45d36b364c643c4
Adam Ure <adamqure@icloud.com>
2019-03-20 14:22:28 -0600

    Add SMS rest test serverlet

--- rest-test-service/src/main/java/password/pwm/resttest/ExternalMacroServlet.java
+++ rest-test-service/src/main/java/password/pwm/resttest/ExternalMacroServlet.java
@@ -23,6 +23,7 @@
 
 package password.pwm.resttest;
 
+import com.google.gson.Gson;
 import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
@@ -33,28 +34,68 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Date;
 
 @WebServlet(
         name = "NewUserServlet",
-        urlPatterns = { "/external/macro1" }
+        urlPatterns = { "/sms", "/macro" }
 )
 
 public class ExternalMacroServlet extends HttpServlet
 {
+    final String USERNAME_PARAMETER = "username";
+    final String SUCCESSFUL = "true";
+    final String UNSUCCESSFUL = "false";
+
     @Override
     protected void doPost( final HttpServletRequest req, final HttpServletResponse resp ) throws ServletException, IOException
     {
+        if (req.getServletPath().equals("/sms")) {
+            SmsResponse instance = SmsResponse.getInstance();
+            final InputStream inputStream = req.getInputStream();
+            final String body = IOUtils.toString( inputStream );
 
-        final InputStream inputStream = req.getInputStream();
-        final String body = IOUtils.toString( inputStream );
+            String[] messageContent = body.split("=");
+            String message = messageContent[messageContent.length - 1];
+            String username = message.split("\\+")[0];
+            Date currentDate = new Date();
+            SmsPostResponseBody messageBody = new SmsPostResponseBody(message, currentDate);
 
-        System.out.println( "input POST body:  " + body );
+            instance.addToMap(username, messageBody);
 
+            System.out.println( "input POST body:  " + body );
 
-        resp.setHeader( "Content-Type", "application/json" );
+            resp.setHeader( "Content-Type", "application/json" );
 
-        final PrintWriter writer = resp.getWriter();
-        writer.write(  "{\"output\":\"macro api output SPLAT!\"}" );
-        writer.close();
+            final PrintWriter writer = resp.getWriter();
+            writer.write(  "{\"output\":\"Message Received\"}" );
+            writer.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getServletPath().equals("/sms")) {
+            //Check request
+            SmsResponse instance = SmsResponse.getInstance();
+            String requestUsername = req.getParameter(USERNAME_PARAMETER);
+            SmsGetResponseBody responseBody;
+
+            //Get body
+            if (instance.getRecentSmsMessages().containsKey(requestUsername)) {
+                SmsPostResponseBody body = instance.getRecentFromMap(requestUsername);
+                responseBody = new SmsGetResponseBody(SUCCESSFUL, body.getMessageContent());
+            } else {
+                responseBody = new SmsGetResponseBody(UNSUCCESSFUL, "");
+            }
+
+            //Send response
+            Gson gson = new Gson();
+            resp.setHeader("Content-Type", "application/json" );
+            final PrintWriter writer = resp.getWriter();
+            writer.write(gson.toJson(responseBody));
+            writer.close();
+
+        }
     }
 }
