b471413f74803868720bae48c0e45c3892960c59
Jason Rivard <jrivard@gmail.com>
2022-01-01 00:24:20 -0500

    add b64 tests

--- server/src/test/java/password/pwm/util/java/StringUtilTest.java
+++ server/src/test/java/password/pwm/util/java/StringUtilTest.java
@@ -44,4 +44,95 @@ public class StringUtilTest
         Assert.assertEquals( original, stripped );
 
     }
+
+    private static byte[] makeB64inputByteArray()
+    {
+        final int byteLength = 256;
+        final byte[] inputArray = new byte[byteLength];
+
+        byte nextByte = 0;
+        for ( int i = 0; i < byteLength; i++ )
+        {
+            inputArray[i] = nextByte;
+            nextByte += i;
+        }
+
+        return inputArray;
+    }
+
+    private static final String B64_TEST = "AAABAwYKDxUcJC03Qk5baXiImau+0uf9FCxFX3qWs9HwEDFTdpq/5Qw0XYey3gs5aJjJ+y5il80EPHWv6iZjoeAgYaPm"
+            + "Km+1/ESN1yJuuwlYqPlLnvJHnfRMpf9athNx0DCR81a6H4XsVL0nkv5r2Ui4KZsOgvdt5FzVT8pGw0HAQMFDxkrPVdxk7XcCjhupOMhZ634Sp"
+            + "z3UbAWfOtZzEbBQ8ZM22n8lzHQdx3Iey3ko2Ik77qJXDcR8Ne+qZiPhoGAh46ZqL/W8hE0X4q57SRjouYteMgfdtIxlPxr207GQcFEzFvrfxa"
+            + "yUfWdSPisZCPjp287Ct62knJWPioaDgQ==";
+
+    private static final String B64_TEST_URL_SAFE = "AAABAwYKDxUcJC03Qk5baXiImau-0uf9FCxFX3qWs9HwEDFTdpq_5Qw0XYey3gs5aJjJ-y5il80EPHWv6iZjoeAgYaPm"
+            + "Km-1_ESN1yJuuwlYqPlLnvJHnfRMpf9athNx0DCR81a6H4XsVL0nkv5r2Ui4KZsOgvdt5FzVT8pGw0HAQMFDxkrPVdxk7XcCjhupOMhZ634Sp"
+            + "z3UbAWfOtZzEbBQ8ZM22n8lzHQdx3Iey3ko2Ik77qJXDcR8Ne-qZiPhoGAh46ZqL_W8hE0X4q57SRjouYteMgfdtIxlPxr207GQcFEzFvrfxa"
+            + "yUfWdSPisZCPjp287Ct62knJWPioaDgQ==";
+
+    private static final String B64_TEST_GZIP = "H4sIAAAAAAAAAAEAAf/+AAABAwYKDxUcJC03Qk5baXiImau+0uf9FCxFX3qWs9HwEDFTdpq/5Qw0XYey3gs5aJjJ+y5il"
+            + "80EPHWv6iZjoeAgYaPmKm+1/ESN1yJuuwlYqPlLnvJHnfRMpf9athNx0DCR81a6H4XsVL0nkv5r2Ui4KZsOgvdt5FzVT8pGw0HAQMFDxkrPVdx"
+            + "k7XcCjhupOMhZ634Spz3UbAWfOtZzEbBQ8ZM22n8lzHQdx3Iey3ko2Ik77qJXDcR8Ne+qZiPhoGAh46ZqL/W8hE0X4q57SRjouYteMgfdtIxlP"
+            + "xr207GQcFEzFvrfxayUfWdSPisZCPjp287Ct62knJWPioaDgR3bmXcAAQAA";
+
+    private static final String B64_TEST_GZIP_URL_SAFE = "H4sIAAAAAAAAAAEAAf_-AAABAwYKDxUcJC03Qk5baXiImau-0uf9FCxFX3qWs9HwEDFTdpq_5Qw0XYey3gs5aJjJ-y5il"
+            + "80EPHWv6iZjoeAgYaPmKm-1_ESN1yJuuwlYqPlLnvJHnfRMpf9athNx0DCR81a6H4XsVL0nkv5r2Ui4KZsOgvdt5FzVT8pGw0HAQMFDxkrPVdx"
+            + "k7XcCjhupOMhZ634Spz3UbAWfOtZzEbBQ8ZM22n8lzHQdx3Iey3ko2Ik77qJXDcR8Ne-qZiPhoGAh46ZqL_W8hE0X4q57SRjouYteMgfdtIxlP"
+            + "xr207GQcFEzFvrfxayUfWdSPisZCPjp287Ct62knJWPioaDgR3bmXcAAQAA";
+
+    @Test
+    public void base64TestEncode() throws Exception
+    {
+        final String b64string = StringUtil.base64Encode( makeB64inputByteArray() );
+        Assert.assertEquals( B64_TEST, b64string );
+    }
+
+    @Test
+    public void base64TestDecode() throws Exception
+    {
+        final byte[] b64array = StringUtil.base64Decode( B64_TEST );
+        Assert.assertArrayEquals( makeB64inputByteArray(), b64array );
+    }
+
+    @Test
+    public void base64TestEncodeUrlSafe() throws Exception
+    {
+        final String b64string = StringUtil.base64Encode( makeB64inputByteArray(), StringUtil.Base64Options.URL_SAFE );
+        Assert.assertEquals( B64_TEST_URL_SAFE, b64string );
+    }
+
+    @Test
+    public void base64TestDecodeUrlSafe() throws Exception
+    {
+        final byte[] b64array = StringUtil.base64Decode( B64_TEST_URL_SAFE, StringUtil.Base64Options.URL_SAFE );
+        Assert.assertArrayEquals( makeB64inputByteArray(), b64array );
+    }
+
+    @Test
+    public void base64TestEncodeGzipAndUrlSafe() throws Exception
+    {
+        final String b64string = StringUtil.base64Encode( makeB64inputByteArray(), StringUtil.Base64Options.URL_SAFE, StringUtil.Base64Options.GZIP );
+        Assert.assertEquals( B64_TEST_GZIP_URL_SAFE, b64string );
+    }
+
+    @Test
+    public void base64TestDecodeGzipAndUrlSafe() throws Exception
+    {
+        final byte[] b64array = StringUtil.base64Decode( B64_TEST_GZIP_URL_SAFE, StringUtil.Base64Options.URL_SAFE, StringUtil.Base64Options.GZIP );
+        Assert.assertArrayEquals( makeB64inputByteArray(), b64array );
+    }
+
+    @Test
+    public void base64TestEncodeGzip() throws Exception
+    {
+        final String b64string = StringUtil.base64Encode( makeB64inputByteArray(), StringUtil.Base64Options.GZIP );
+        Assert.assertEquals( B64_TEST_GZIP, b64string );
+    }
+
+    @Test
+    public void base64TestDecodeGzip() throws Exception
+    {
+        final byte[] b64array = StringUtil.base64Decode( B64_TEST_GZIP, StringUtil.Base64Options.GZIP );
+        Assert.assertArrayEquals( makeB64inputByteArray(), b64array );
+    }
 }
